0a50d3e9040d31208bbf2e3925df0809aef90a8a
Svetlana Terenteva <gubinas83@gmail.com>
2021-11-03 19:26:07 +0300

    Fix routes

--- src/express/express.js
+++ src/express/express.js
@@ -4,7 +4,7 @@ const path = require(`path`);
 const express = require(`express`);
 const DEFAULT_PORT = 8080;
 const mainRoutes = require(`./routes/main-routes`);
-const myRoutes = require(`./routes/main-routes`);
+const myRoutes = require(`./routes/my-routes`);
 const articlesRoutes = require(`./routes/articles-routes`);
 const PUBLIC_DIR = `public`;
 
