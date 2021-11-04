2ecb7cefb9f7d50c276cce6a431a90c049f1ba87
Svetlana Terenteva <gubinas83@gmail.com>
2021-11-05 00:00:33 +0300

    task-3.19

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,61 +1,35 @@
 "use strict";
 
 const fs = require(`fs`).promises;
-const http = require(`http`);
-const chalk = require(`chalk`);
 const DEFAULT_PORT = 3000;
 const mockFile = `mock.json`;
-const {HttpCode} = require(`../../constants`);
+const express = require(`express`);
+
+const app = express();
+
+app.use(express.json());
+
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(mockFile);
+    const posts = JSON.parse(fileContent);
+    res.json(posts);
+  } catch (err) {
+    res.send([]);
+  }
+});
 
 module.exports = {
   name: `--server`,
   run(args) {
-    const [portNum] = args;
-    const port = Number.parseInt(portNum, 10) || DEFAULT_PORT;
+    const [userPort] = args;
+    const port = Number.parseInt(userPort, 10) || DEFAULT_PORT;
 
-    const onServerCreate = async (req, res) => {
-      const notFoundMessage = `Not found`;
-
-      const getResponse = (response, httpCode, message) => {
-        const template = `<!DOCTYPE html>
-        <html lang="ru">
-        <head>
-        <title>Hello from node</title>
-        </head>
-        <body>
-        ${message}
-        </body></html>`.trim();
-
-        response.writeHead(httpCode, {
-          "Content-type": `text/html; charset=UTF-8 `
-        });
-
-        response.end(template);
-      };
-
-      switch (req.url) {
-        case `/`:
-          const content = await fs.readFile(mockFile);
-          const items = JSON.parse(content);
-          const message = items.map((item) => `<li>${item.title}</li>`).join(``);
-          getResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-          break;
-        default:
-          getResponse(res, HttpCode.NOT_FOUND, notFoundMessage);
-          break;
+    app.listen(port, (err) => {
+      if (err) {
+        console.info(err);
       }
-    };
-
-    const httpServer = http.createServer(onServerCreate);
-    httpServer.listen(port)
-              .on(`listening`, (err) => {
-                if (err) {
-                  console.error(chalk.red(err));
-                }
-                console.info(chalk.green(`Ожидаю соединений на ${port}`));
-              })
-              .on(`error`, ({message}) => {
-                console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
-              });
+    });
   }
 };
+
